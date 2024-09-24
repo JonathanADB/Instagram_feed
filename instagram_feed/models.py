94cf1546ea719533a89e951c859b167dddc9ef3d
Jonathan Dominguez <150841815+JonathanADB@users.noreply.github.com>
2024-09-24 13:42:13 +0200

    Update models.py

--- instagram_feed/models.py
+++ instagram_feed/models.py
@@ -6,29 +6,27 @@ import magic  #librería para la validación de MIME types.
 
 class InstagramAccount(models.Model):
     username = models.CharField(max_length=50)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     last_updated = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.username
 
-
 class InstagramPost(models.Model):
     account = models.ForeignKey(InstagramAccount, on_delete=models.CASCADE)
     post_id = models.CharField(max_length=50)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.account.username} - {self.post_id}"
 
     def get_image_urls(self):
-        """Retorna las URLs de las imágenes asociadas al post."""
         return [media.media_url.url for media in self.media.filter(media_type='image')]
 
     def get_video_urls(self):
-        """Retorna las URLs de los videos asociados al post."""
         return [media.media_url.url for media in self.media.filter(media_type='video')]
-
 
 class InstagramMedia(models.Model):
     MEDIA_TYPE_CHOICES = [
@@ -44,16 +42,13 @@ class InstagramMedia(models.Model):
         return f"Media ({self.media_type}) for post {self.post.post_id}"
 
     def clean(self):
-        """Valida el tipo MIME del archivo subido para asegurarse de que coincide con el tipo de media."""
         mime = magic.Magic(mime=True)
         file_mime_type = mime.from_buffer(self.media_url.open().read())
-        self.media_url.close()  # Cerramos el archivo tras la lectura para evitar problemas posteriores.
+        self.media_url.close()
 
-        # Definir los tipos MIME válidos para cada tipo de media.
         valid_image_mime_types = ['image/jpeg', 'image/png', 'image/jpg']
         valid_video_mime_types = ['video/mp4', 'video/quicktime']
 
-        # Realizar la validación de acuerdo al tipo de media.
         if self.media_type == 'image' and file_mime_type not in valid_image_mime_types:
             raise ValidationError(
                 f"Invalid file type for image. Supported types are: {', '.join(valid_image_mime_types)}")
@@ -63,7 +58,7 @@ class InstagramMedia(models.Model):
                 f"Invalid file type for video. Supported types are: {', '.join(valid_video_mime_types)}")
 
     def save(self, *args, **kwargs):
-        """Sobrescribe el método save para validar antes de guardar."""
-        # Limpieza y validación automática del archivo antes de guardarlo.
-        self.full_clean()  # full_clean() es preferible para llamar a clean() y validar el modelo completo.
+        self.full_clean()
         super().save(*args, **kwargs)
+
+
